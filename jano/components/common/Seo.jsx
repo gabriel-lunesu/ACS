4f68ec96d5f46aa66cdecb80d8d8291130dc88d3
gabriel-lunesu <gabriel@lunesu.com>
2023-05-10 01:57:48 +0200

    Update Seo.jsx

--- jano/components/common/Seo.jsx
+++ jano/components/common/Seo.jsx
@@ -5,7 +5,7 @@ const Seo = ({ pageTitle }) => (
     <Head>
       <title>
         {pageTitle &&
-          `${pageTitle} || Advanced Crypto Signals}`}
+          `${pageTitle} || Advanced Crypto Signals`}
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
